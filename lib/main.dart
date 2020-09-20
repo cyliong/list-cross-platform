b2c5903db477b6a7b04e5f2117dcd848800c9c00
cyliong <liongtheprogrammer@gmail.com>
2020-09-20 13:03:28 +0800

    Simplify the main() method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:items/page/home_page.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(App());
-}
+void main() => runApp(App());
 
 class App extends StatelessWidget {
   App({Key key}) : super(key: key);
