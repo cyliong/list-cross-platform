70fddab161ff1cd18dd05653ff0e551b70aa6514
cyliong <liongtheprogrammer@gmail.com>
2020-10-15 21:28:53 +0800

    Simplify database race condition solution

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,23 +1,16 @@
 import 'package:sqflite/sqflite.dart';
-import 'package:synchronized/synchronized.dart';
 
 class DatabaseHelper {
   static const databaseName = 'app.db';
 
-  static final _lock = Lock();
+  static Future<Database> _database;
 
-  static Database _database;
-
-  static Future<Database> get database async {
+  static Future<Database> get database {
     if (_database == null) {
-      await _lock.synchronized(() async {
-        if (_database == null) {
-          _database = await openDatabase(databaseName, version: 1,
-              onCreate: (db, version) async {
-            await db.execute(
-                'CREATE TABLE list_item (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL)');
-          });
-        }
+      _database =
+          openDatabase(databaseName, version: 1, onCreate: (db, version) async {
+        await db.execute(
+            'CREATE TABLE list_item (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL)');
       });
     }
     return _database;
