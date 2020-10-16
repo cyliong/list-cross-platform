45d9f81ead34b52f2d6eded4fe436f9d4545c83e
cyliong <liongtheprogrammer@gmail.com>
2020-10-16 20:49:01 +0800

    Implement singleton pattern with factory constructor

--- lib/model/active_record.dart
+++ lib/model/active_record.dart
@@ -23,28 +23,28 @@ abstract class ActiveRecord {
   }
 
   Future<int> insert() async {
-    final database = await DatabaseHelper.database;
+    final database = await DatabaseHelper().database;
     final id = await database.insert(tableName, toMap());
     if (id > 0) this.id = id;
     return id;
   }
 
-  Future<int> update() => DatabaseHelper.database.then((database) => database
+  Future<int> update() => DatabaseHelper().database.then((database) => database
       .update(tableName, toMap(), where: '$idColumn = ?', whereArgs: [id]));
 
   static Future<int> delete(String tableName, int id, [String idColumn]) async {
-    final database = await DatabaseHelper.database;
+    final database = await DatabaseHelper().database;
     return database.delete(tableName,
         where: '${_idColumnName(idColumn)} = ?', whereArgs: [id]);
   }
 
   static Future<int> deleteAll(String tableName) =>
-      DatabaseHelper.database.then((database) => database.delete(tableName));
+      DatabaseHelper().database.then((database) => database.delete(tableName));
 
   static Future<T> find<T extends ActiveRecord>(
       String tableName, int id, T createModel(Map<String, dynamic> map),
       [String idColumn]) async {
-    final database = await DatabaseHelper.database;
+    final database = await DatabaseHelper().database;
     final maps = await database.query(tableName,
         where: '${_idColumnName(idColumn)} = ?', whereArgs: [id], limit: 1);
     return maps.length == 0 ? null : createModel(maps.first);
@@ -52,7 +52,7 @@ abstract class ActiveRecord {
 
   static Future<List<T>> findAll<T extends ActiveRecord>(
       String tableName, T createModel(Map<String, dynamic> map)) async {
-    final database = await DatabaseHelper.database;
+    final database = await DatabaseHelper().database;
     final maps = await database.query(tableName);
     return maps.map((m) => createModel(m)).toList();
   }
