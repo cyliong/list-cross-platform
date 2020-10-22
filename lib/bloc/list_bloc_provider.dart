60a17ad5fcd56f16a94fe8bbc5e234c161e2c54c
cyliong <liongtheprogrammer@gmail.com>
2020-10-22 10:20:53 +0800

    Implement ListBlocProvider class by extending InheritedWidget

---
+++ lib/bloc/list_bloc_provider.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+import 'package:items/bloc/list_bloc.dart';
+
+class ListBlocProvider extends InheritedWidget {
+  final ListBloc listBloc;
+
+  const ListBlocProvider({Key key, Widget child, this.listBloc})
+      : super(key: key, child: child);
+
+  static ListBlocProvider of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ListBlocProvider>();
+  }
+
+  @override
+  bool updateShouldNotify(ListBlocProvider old) => listBloc != old.listBloc;
+}
