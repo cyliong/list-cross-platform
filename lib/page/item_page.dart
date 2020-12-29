42f393ba7b01d0121f2676175f2f94dcde2a22e6
cyliong <liongtheprogrammer@gmail.com>
2020-12-29 21:22:00 +0800

    Align form fields to left

--- lib/page/item_page.dart
+++ lib/page/item_page.dart
@@ -29,6 +29,7 @@ class _ItemPageState extends State<ItemPage> {
         child: Padding(
           padding: EdgeInsets.all(16.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextField(
                 key: Key('item_text_field'),
