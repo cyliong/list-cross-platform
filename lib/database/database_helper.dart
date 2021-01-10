d701f50e5c964b41644cad23f9f4a881cf873e73
cyliong <liongtheprogrammer@gmail.com>
2021-01-10 22:53:35 +0800

    Refactor database creation method

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -11,11 +11,16 @@ class DatabaseHelper {
 
   Future<Database> get database {
     if (_database == null) {
-      _database =
-          openDatabase(databaseName, version: 1, onCreate: (db, version) async {
-        await db.execute(
-            'CREATE TABLE list_item (id INTEGER PRIMARY KEY NOT NULL, title TEXT NOT NULL)');
-      });
+      _database = openDatabase(
+        databaseName,
+        version: 1,
+        onCreate: (db, version) async {
+          await db.execute('''CREATE TABLE list_item (
+            id INTEGER PRIMARY KEY NOT NULL,
+            title TEXT NOT NULL
+          )''');
+        },
+      );
     }
     return _database;
   }
