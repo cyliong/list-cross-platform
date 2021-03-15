385030005fae8fb3f17d4e769404d29ce0ca996a
cyliong <liongtheprogrammer@gmail.com>
2021-03-15 12:44:22 +0800

    Refactor the DatabaseHelper class to support null safety

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,5 +1,3 @@
-// @dart=2.9
-
 import 'package:sqflite/sqflite.dart';
 
 class DatabaseHelper {
@@ -9,7 +7,7 @@ class DatabaseHelper {
   factory DatabaseHelper() => _instance;
   DatabaseHelper._();
 
-  Future<Database> _database;
+  Future<Database>? _database;
 
   Future<Database> get database {
     if (_database == null) {
@@ -30,6 +28,6 @@ class DatabaseHelper {
         },
       );
     }
-    return _database;
+    return _database!;
   }
 }
