d956323da14599d0b9673af2da40fcd38773f44d
cyliong <liongtheprogrammer@gmail.com>
2021-04-03 14:51:44 +0800

    Add the isConfirmDelete() method to SettingsRepository

--- lib/repository/settings_repository.dart
+++ lib/repository/settings_repository.dart
@@ -4,4 +4,9 @@ class SettingsRepository {
   static const String _confirmDeletePrefsKey = 'confirm_delete';
 
   final _prefsFuture = SharedPreferences.getInstance();
+
+  Future<bool> isConfirmDelete() async {
+    final prefs = await _prefsFuture;
+    return prefs.getBool(_confirmDeletePrefsKey) ?? true;
+  }
 }
