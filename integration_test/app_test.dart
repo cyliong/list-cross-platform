e7c6f85f0e5e2153ce8a642e285ce2ed23732cb3
cyliong <liongtheprogrammer@gmail.com>
2021-04-13 21:21:55 +0800

    Migrate test code

---
+++ integration_test/app_test.dart
@@ -0,0 +1,39 @@
+// @dart=2.9
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:items/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  final listTitleFinder = find.byWidgetPredicate((widget) =>
+      widget is AppBar &&
+      widget.title is Text &&
+      (widget.title as Text).data == 'List');
+  final addButtonFinder = find.byKey(Key('add_button'));
+  final saveButtonFinder = find.byKey(Key('save_button'));
+  final itemTextFieldFinder = find.byKey(Key('item_text_field'));
+
+  testWidgets('Add an item to the list', (tester) async {
+    const newItem = 'Item 1';
+
+    app.main();
+    await tester.pumpAndSettle();
+
+    // Given I am on the list page
+    expect(listTitleFinder, findsOneWidget);
+
+    // When I add a new item
+    await tester.tap(addButtonFinder);
+    await tester.pumpAndSettle();
+    await tester.tap(itemTextFieldFinder);
+    await tester.enterText(itemTextFieldFinder, newItem);
+    await tester.tap(saveButtonFinder);
+    await tester.pumpAndSettle();
+
+    // Then I should see the new item on the list
+    expect(find.text(newItem), findsOneWidget);
+  });
+}
