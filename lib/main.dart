6245da74242ac1c875f5dea2703853a26199683b
cyliong <liongtheprogrammer@gmail.com>
2021-09-26 21:28:42 +0800

    Use the const App widget in main()

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:items/bloc/list_bloc.dart';
 import 'package:items/bloc/list_bloc_provider.dart';
 import 'package:items/page/home_page.dart';
 
-void main() => runApp(App());
+void main() => runApp(const App());
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
