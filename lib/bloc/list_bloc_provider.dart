9899ba8b2665dc022094d5c643b38d9710a8b0f1
cyliong <liongtheprogrammer@gmail.com>
2021-10-03 12:37:28 +0800

    Reformat ListBlocProvider

--- lib/bloc/list_bloc_provider.dart
+++ lib/bloc/list_bloc_provider.dart
@@ -2,8 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:items/bloc/list_bloc.dart';
 
 class ListBlocProvider extends InheritedWidget {
-  final ListBloc listBloc;
-
   const ListBlocProvider(
       {Key? key, required Widget child, required this.listBloc})
       : super(key: key, child: child);
@@ -14,6 +12,8 @@ class ListBlocProvider extends InheritedWidget {
     assert(listBlocProvider != null, 'No ListBlocProvider found in context');
     return listBlocProvider!;
   }
+
+  final ListBloc listBloc;
 
   @override
   bool updateShouldNotify(ListBlocProvider old) => listBloc != old.listBloc;
