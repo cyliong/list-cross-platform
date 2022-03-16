224a0f7a89324a59f86c45cf7a19b91497c7f7ae
cyliong <liongtheprogrammer@gmail.com>
2022-03-16 19:01:11 +0800

    Remove unused BLoC code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:items/bloc/list_bloc.dart';
-import 'package:items/bloc/list_bloc_provider.dart';
 import 'package:items/page/home_page.dart';
 
 void main() => runApp(const App());
@@ -13,10 +11,7 @@ class App extends StatelessWidget {
     return MaterialApp(
       title: 'Items',
       theme: ThemeData.dark(),
-      home: ListBlocProvider(
-        listBloc: ListBloc(),
-        child: const HomePage(title: 'List'),
-      ),
+      home: const HomePage(title: 'List'),
       debugShowCheckedModeBanner: false,
     );
   }
